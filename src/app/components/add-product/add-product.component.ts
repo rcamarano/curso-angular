45b93eeb148a269f601803bb701d853660566a2e
rcamarano <rogeriocamarano@gmail.com>
2024-03-14 12:29:40 -0300

    addProduct component created, rendering addProduct form and conecting with api but still sending empty object

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { ProductListService } from '../../services/product-list.service';
 
 @Component({
   selector: 'app-add-product',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './add-product.component.css'
 })
 export class AddProductComponent implements OnInit {
+  productForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      title: '',
+      description: '',
+      price: 0,
+      discountPercentage: 0,
+      rating: 0,
+      stock: 0,
+      brand: '',
+      category: '',
+    });
   }
 
+  async addProduct(): Promise<void> {
+    const product = this.productForm.value;
+    // ... add product to the database
+    fetch('https://dummyjson.com/products/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        title: '',
+        description: '',
+        price: 0,
+        discountPercentage: 0,
+        rating: 0,
+        stock: 0,
+        brand: '',
+        category: '',
+      })
+    })
+    .then(res => res.json())
+    .then(console.log);
+  }
 }
--- src/app/services/product-list.service.ts
+++ src/app/services/product-list.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 import { Products } from '../interfaces/Products';
 
 
@@ -18,4 +19,30 @@ export class ProductListService {
     console.log(products);
     return products;
   }
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  // async addProduct(newProduct: Products): Promise<any> {
+  //   return this.http.post<any>(this.apiProductsUrl, newProduct)
+  //     .pipe(
+  //       catchError(error => {
+  //         throw new Error('Erro ao adicionar novo produto: ' + error.message);
+  //       })
+  //     );
+  // }
+  addProduct(product: Products): Observable<Products> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.post<Products>(this.apiProductsUrl, product)
+      .pipe(
+        catchError(this.handleError('addProduct', product))
+      );
+}
 }
